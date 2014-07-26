26b6900b5c2678f267a6bd31565d27b04422ab3e
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2014-07-26 10:30:46 +0200

    upgraded file options validator

--- smrt-gulp-r/index.js
+++ smrt-gulp-r/index.js
@@ -7,10 +7,11 @@
 "use strict";
 
 var path = require("path"),
+    _ = require("lodash"),
     gutil = require("gulp-util"),
     OptimizerSettings = require(path.join(__dirname, "/OptimizerSettings")),
-    PluginError = gutil.PluginError,
     PLUGIN_NAME = "gulp-r",
+    PluginError = gutil.PluginError,
     Q = require("q"),
     requirejs = require("requirejs"),
     through = require("through2");
@@ -23,7 +24,9 @@ var path = require("path"),
  * @returns {Promise} resolved if handled successfully, rejected otherwise
  */
 function handleFileStream(stream, file, encoding, options) {
-    return optimizeFile(file, encoding, options);
+    var doOptimizeFile = _.curry(optimizeFile)(file, encoding, options);
+
+    return normalizeFileOptions(file, encoding, options).then(doOptimizeFile);
 }
 
 /**
@@ -44,35 +47,67 @@ function handleNullStream(stream, file) {
 /**
  * @param {*} file through2 file stream
  * @param {String} encoding file encoding
- * @param {smrt-gulp-r/OptimizerSettings} options valid optimizer options
- * @returns {Promise} resolved if optimized successfully, rejected otherwise
+ * @param {Object} options user input configuration
+ * @returns {Promise} resolved if options are valid, rejected otherwise
  */
-function optimizeFile(file, encoding, options) {
-    var fileOptions,
-        filePath,
+function normalizeFileOptions(file, encoding, options) {
+    var fileOptions = _.clone(options),
         include = path.basename(file.path).replace(path.extname(file.path), ""),
-        optimizeFileDeferred = Q.defer(),
-        optimizeFilePromise = optimizeFileDeferred.promise;
+        normalizeFileOptionsDeferred = Q.defer(),
+        normalizeFileOptionsPromise = normalizeFileOptionsDeferred.promise;
 
-    fileOptions = {
-        "baseUrl": options.baseUrl,
-        "include": include,
-        "out": function (text, sourceMapText) {
-            // console.log(sourceMapText);
-            file.contents = new Buffer(text, encoding);
-        },
-        "paths": options.paths
+    fileOptions.out = function (text, sourceMapText) {
+        // console.log(sourceMapText);
+        file.contents = new Buffer(text, encoding);
     };
 
-    if (options.name) {
-        fileOptions.name = path.relative(options.baseUrl, options.name);
+    if (fileOptions.name) {
+        fileOptions.name = path.relative(fileOptions.baseUrl, fileOptions.name);
+        fileOptions.include = include;
+    } else {
+        fileOptions.name = include;
     }
+
+    normalizeFileOptionsDeferred.resolve(fileOptions);
+
+    return normalizeFileOptionsPromise;
+}
+
+/**
+ * @param {Object} options user input configuration
+ * @returns {Promise} resolved if options are valid, rejected otherwise
+ */
+function normalizeStreamOptions(options) {
+    var normalizeStreamOptionsDeferred = Q.defer(),
+        normalizeStreamOptionsPromise = normalizeStreamOptionsDeferred.promise;
+
+    try {
+        normalizeStreamOptionsDeferred.resolve(new OptimizerSettings(options));
+    } catch (err) {
+        normalizeStreamOptionsDeferred.reject(err);
+    }
+
+    return normalizeStreamOptionsPromise;
+}
+
+/**
+ * @param {*} file through2 file stream
+ * @param {String} encoding file encoding
+ * @param {smrt-gulp-r/OptimizerSettings} options valid optimizer options
+ * @param {smrt-gulp-r/OptimizerSettings} fileOptions valid file options
+ * @returns {Promise} resolved if optimized successfully, rejected otherwise
+ */
+function optimizeFile(file, encoding, options, fileOptions) {
+    var optimizeFileDeferred = Q.defer(),
+        optimizeFilePromise = optimizeFileDeferred.promise;
 
     function errback(err) {
         optimizeFileDeferred.reject(err);
     }
 
     try {
+        // r.js goes nuts sometimes without changing CWD
+        // this prevents some edge case failures
         process.chdir(options.baseUrl);
     } catch (err) {
         errback(err);
@@ -87,25 +122,8 @@ function optimizeFile(file, encoding, options) {
     return optimizeFilePromise;
 }
 
-/**
- * @param {Object} options user input configuration
- * @returns {Promise} resolved if options are valid, rejected otherwise
- */
-function validateOptions(options) {
-    var validateOptionsDeferred = Q.defer(),
-        validateOptionsPromise = validateOptionsDeferred.promise;
-
-    try {
-        validateOptionsDeferred.resolve(new OptimizerSettings(options));
-    } catch (err) {
-        validateOptionsDeferred.reject(err);
-    }
-
-    return validateOptionsPromise;
-}
-
 module.exports = function (options) {
-    var validateOptionsPromise = validateOptions(options);
+    var normalizeStreamOptionsPromise = normalizeStreamOptions(options);
 
     return through.obj(function (file, encoding, callback) {
         var that = this;
@@ -118,7 +136,7 @@ module.exports = function (options) {
             callback(null, optimized);
         }
 
-        validateOptionsPromise
+        normalizeStreamOptionsPromise
             .then(function (validatedOptions) {
                 if (file.isNull()) {
                     return handleNullStream(that, file, encoding, validatedOptions);
